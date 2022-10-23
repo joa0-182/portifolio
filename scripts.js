2188074a54dcbd2ab7a76d17e1cc4b5096d1988e
joa0-182 <Joaopedromaria182@gmail.com>
2022-10-23 15:03:32 -0300

    estruturas e alterações

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+
+const habilidades = document.querySelectorAll('.skill-box');
+const descricao = document.querySelector('.texto-descricao');
+
+const sobreHabilidades =  [
+                            '<h3>HTML</h3> <br> <p>Linguagem de HiperTexto, sendo o componente base da web, usando assim para construções de Websites e Inserção de Novos conteudos.</p> <br> <p>Nivel: Intermediário.</p> <br>',
+                            '<h3>Css</h3> <br> <p>Mecanismo para adicionar estilo a um documento web.</p> <br> <p>Nivel: Mediano.</p>',
+                            '<h3>JavaScript</h3> <br> <p>Linguagem de programação que permite a você implementar itens complexos em páginas web.</p> <br> <p>Nivel: Iniciante</p>',
+                            '<h3>BootStrap</h3> <br> <p>framework front-end que fornece estruturas de CSS para a criação de sites e aplicações responsivas de forma rápida e simples.</p> <br>',
+                            '<h3>MySQL</h3> <br> <p>sistema de gerenciamento de banco de dados relacionais (RBDMs) de código aberto (open source) com um funcionamento baseado em um modelo do tipo cliente-servidor.</p> <br> <p>Nível: Mediano</p>',
+                            '<h3>Delphi</h3> <br> <p>Interface de desenvolvimento integrado (IDE) e é usado para o desenvolvimento de sistemas de inúmeras áreas de atuação. </p> <br> <p>Nivel: Mediano </p>',
+                            '<h3>c#</h3> <br> <p>Linguagem de programação orientada a objetos, que foi desenvolvida pela Microsoft e faz parte da plataforma . NET.</p> <br> <p>Nivel: Inciante </p>',
+                            '<h3></h3> <br> <p>plataforma da Microsoft para o desenvolvimento de aplicações Web e é o sucessor da tecnologia ASP.</p> <br> <p>Nivel: Iniciante </p>',
+                            '<h3>Ionic</h3> <br> <p>framework open source para desenvolvimento de aplicativos móveis multiplataforma.</p> <br> <p>Nivel: Inciante </p>'];
+
+
